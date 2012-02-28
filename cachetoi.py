1a606e94c0e71bc9e674cb45c366bfb9041bd047
Revence Kalibwani <revence@1st.ug>
2012-02-28 17:28:13 +0300

    Cache-Toi, initial commit.

---
+++ cachetoi.py
@@ -0,0 +1,44 @@
+# encoding: UTF-8
+# cachetoi.py
+# 
+# Cache-toi !
+# French for “Hide!”
+
+from Crypto.Hash import SHA
+from datetime import datetime
+import re
+
+class CacheToi:
+  def get(self, *args, **kwargs):
+    answerer, do_once = None, False
+    try:
+      answerer  = self.get_once
+      do_once   = True
+    except AttributeError:
+      try:
+        answerer  = self.cached_get
+      except AttributeError:
+        self.response.set_status(500)
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write('Implement cache-aware methods: get_once/cached_get, latest. Cache me if you can.')
+        return
+    self.response.headers['Cache-Control'] = 'public'
+    if do_once:
+      if self.request.headers.get('If-None-Match') or self.request.headers.get('If-Modified-Since'):
+        self.response.set_status(304)
+        return
+    latest  = datetime.now() if do_once else self.latest(*args, **kwargs)
+    etag    = SHA.new(str(latest)).hexdigest()   # Rubythonic!
+    self.response.headers['ETag'] = etag
+    if etag == self.request.headers.get('If-None-Match'):
+      self.response.set_status(304)
+      return
+    if self.request.headers.get('If-None-Match'):
+      temps = self.request.headers.get('If-None-Match')
+      match = re.match(r'[^ ]+ (\d+) (\w+) (\d+) (\d+):(\d+):(\d+)', temps)
+      if match:
+        previously  = datetime(day = int(got.group(1)), month = 'Months: Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec'.split(' ').index(got.group(2)), year = int(got.group(3)), hour = int(got.group(4)), minute = int(got.group(5)), second = int(got.group(6)), microsecond = latest.microsecond)
+        if previously >= latest:
+          self.response.set_status(304)
+          return
+    answerer(*args, **kwargs)
